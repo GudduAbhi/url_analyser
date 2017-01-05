5f1685ed917dcf88424de2cc0c9ee06b0ac9b47d
GudduAbhi <abhishek.upadhyaya3@gmail.com>
2017-01-04 19:28:55 -0500

    Add base64 encoder to shorten a long URL

---
+++ base62encoder.py
@@ -0,0 +1,33 @@
+''' base62 encoding in python for url shortner'''
+
+import string
+from math import floor
+
+
+def toBase62(num, b = 62):
+    if b <= 0 or b > 62:
+        return 0
+    base = string.ascii_lowercase + string.ascii_uppercase + string.digits
+    r = num % b
+    res = base[r];
+    q = floor(num / b)
+    while q:
+        r = q % b
+        q = floor(q / b)
+        res = base[int(r)] + res
+    return res
+
+def toBase10(num, b = 62):
+    base = string.ascii_lowercase + string.ascii_uppercase + string.digits
+    limit = len(num)
+    res = 0
+    for i in range(limit):
+        res = b * res + base.find(num[i])
+    return res
+
+
+if __name__ == '__main__':
+    myencoding = toBase62(10)
+    decodedvalue = toBase10(myencoding)
+    print ('encoded result',myencoding)
+    print ('decoded result',decodedvalue)
